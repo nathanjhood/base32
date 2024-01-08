2875a87c0797569cf385b3bcdcc54cc503cb553b
StoneyDSP <nathanjhood@googlemail.com>
2024-01-08 03:23:07 +0100

    Added README.md
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- include/base32/version.h
+++ include/base32/version.h
@@ -1,5 +1,4 @@
-// base32 v1.0.0.HEAD
-
+// base32 v1.0.0.b9e8cab3366dc8ec19ea67c3e69b5c9cac6af960
 // Version control info
 
 #ifndef base32_VERSION_H_
@@ -8,10 +7,9 @@
 #define base32_VERSION_MAJOR "1"
 #define base32_VERSION_MINOR "0"
 #define base32_VERSION_PATCH "0"
-#define base32_VERSION_TWEAK "init"
-
-#define base32_VERSION "1.0.0.init"
-
-#undef base32_REPOSITORY
+#define base32_VERSION_TWEAK "b9e8cab3366dc8ec19ea67c3e69b5c9cac6af960"
+#define base32_VERSION "1.0.0.b9e8cab3366dc8ec19ea67c3e69b5c9cac6af960"
+#define base32_REPOSITORY "https://github.com/nathanjhood/base32.git"
 
 #endif // base32_VERSION_H_
+
